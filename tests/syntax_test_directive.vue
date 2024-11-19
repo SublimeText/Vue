44f7af8d17cde88018ebc31a8962f43f72de02e3
deathaxe <deathaxe82@googlemail.com>
2024-11-19 15:01:57 +0100

    Add directive keywords

---
+++ tests/syntax_test_directive.vue
@@ -0,0 +1,197 @@
+// SYNTAX TEST "Vue Component.sublime-syntax"
+
+<!--
+ Conditional Rendering
+
+ https://vuejs.org/guide/essentials/conditional.html#conditional-rendering
+ -->
+
+<div v-if="seen"></div>
+//^^^^^^^^^^^^^^^^^^^^^ meta.tag
+//   ^^^^^^^^^^^ meta.attribute-with-value.directive.html
+//   ^^^^ entity.other.attribute-name.html keyword.control.conditional.if.vue
+//       ^ punctuation.separator.key-value.html
+//        ^^^^^^ meta.string.html
+//        ^ string.quoted.double.html punctuation.definition.string.begin.html
+//         ^^^^ meta.interpolation.vue source.js.embedded.vue variable.other.readwrite.js
+//             ^ string.quoted.double.html punctuation.definition.string.end.html
+
+<div v-else-if="maybe + seen"></div>
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.tag
+//   ^^^^^^^^^^^ meta.attribute-with-value.directive.html
+//   ^^^^^^^^^ entity.other.attribute-name.html keyword.control.conditional.elseif.vue
+//            ^ punctuation.separator.key-value.html
+//             ^^^^^^^^^^^^^^ meta.string.html
+//             ^ string.quoted.double.html punctuation.definition.string.begin.html
+//              ^^^^^^^^^^^^ meta.interpolation.vue source.js.embedded.vue
+//              ^^^^^ variable.other.readwrite.js
+//                    ^ keyword.operator.arithmetic.js
+//                      ^^^^ variable.other.readwrite.js
+//                          ^ string.quoted.double.html punctuation.definition.string.end.html
+
+<div v-else></div>
+//^^^^^^^^^^^^^^^^ meta.tag
+//   ^^^^^^ meta.attribute-with-value.directive.html entity.other.attribute-name.html keyword.control.conditional.else.vue
+
+<div v-show="visible"></div>
+//^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.tag
+//   ^^^^^^^^^^^^^^^^ meta.attribute-with-value.directive.html
+//   ^^^^^^ entity.other.attribute-name.html keyword.control.conditional.show.vue
+//         ^ punctuation.separator.key-value.html
+//          ^^^^^^^^^ meta.string.html
+//          ^ string.quoted.double.html punctuation.definition.string.begin.html
+//           ^^^^^^^ meta.interpolation.vue source.js.embedded.vue variable.other.readwrite.js
+//                  ^ string.quoted.double.html punctuation.definition.string.end.html
+
+
+<!--
+ List Rendering
+
+ https://vuejs.org/guide/essentials/list.html#list-rendering
+ -->
+
+<li v-for="todo in todos"> {{ todo.name }} </li>
+//^^^^^^^^^^^^^^^^^^^^^^^^ meta.tag
+//  ^^^^^^^^^^^^^^^^^^^^^ meta.attribute-with-value.directive.html
+//  ^^^^^ entity.other.attribute-name.html keyword.control.loop.for.vue
+//       ^ punctuation.separator.key-value.html
+//        ^^^^^^^^^^^^^^^ meta.string.html
+//        ^ string.quoted.double.html punctuation.definition.string.begin.html
+//         ^^^^^^^^^^^^^ meta.interpolation.vue source.js.embedded.vue
+//         ^^^^ variable.other.readwrite.js
+//              ^^ keyword.operator.js
+//                 ^^^^^ variable.other.readwrite.js
+//                      ^ string.quoted.double.html punctuation.definition.string.end.html
+//                       ^ punctuation.definition.tag.end.html
+//                         ^^^^^^^^^^^^^^^ meta.interpolation.vue
+//                         ^^ punctuation.section.interpolation.begin.html
+//                           ^^^^^^^^^^^ source.js.embedded.vue
+//                            ^^^^ variable.other.readwrite.js
+//                                ^ punctuation.accessor.js
+//                                 ^^^^ meta.property.object.js
+//                                      ^^ punctuation.section.interpolation.end.html
+//                                         ^^^^^ meta.tag
+
+
+<!--
+ Directive Arguments
+
+ https://vuejs.org/guide/essentials/template-syntax.html#arguments
+ -->
+
+<div v-on:event="doSomething()"></div>
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.tag
+//   ^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.attribute-with-value.directive.html
+//   ^^^^^^^^^^ entity.other.attribute-name.html - entity.other entity.other
+//   ^^^^ keyword.control.directive.vue
+//       ^ punctuation.separator.vue
+//        ^^^^^ variable.parameter.vue
+//             ^ punctuation.separator.key-value.html
+//              ^^^^^^^^^^^^^^^ meta.string.html
+//              ^ string.quoted.double.html punctuation.definition.string.begin.html
+//               ^^^^^^^^^^^^^ meta.interpolation.vue source.js.embedded.vue meta.function-call
+//                            ^ meta.string.html string.quoted.double.html punctuation.definition.string.end.html
+
+<div @event="doSomething()"></div>
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.tag
+//   ^^^^^^^^^^^^^^^^^^^^^^ meta.attribute-with-value.directive.html
+//   ^^^^^^ entity.other.attribute-name.html - entity.other entity.other
+//   ^ keyword.control.directive.vue
+//    ^^^^^ variable.parameter.vue
+//         ^ punctuation.separator.key-value.html
+//          ^^^^^^^^^^^^^^^ meta.string.html
+//          ^ string.quoted.double.html punctuation.definition.string.begin.html
+//           ^^^^^^^^^^^^^ meta.interpolation.vue source.js.embedded.vue meta.function-call
+//                        ^ meta.string.html string.quoted.double.html punctuation.definition.string.end.html
+
+
+<!--
+ Dynamic Directive Arguments
+
+ https://vuejs.org/guide/essentials/template-syntax.html#dynamic-arguments
+ -->
+
+<div v-on:[eventName]="doSomething()"></div>
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.tag
+//   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.attribute-with-value.directive.html
+//   ^^^^^ entity.other.attribute-name.html
+//   ^^^^ keyword.control.directive.vue
+//       ^ punctuation.separator.vue
+//        ^^^^^^^^^^^ meta.interpolation.vue
+//        ^ punctuation.section.interpolation.begin.vue
+//         ^^^^^^^^^ source.js.embedded.vue variable.other.readwrite.js
+//                  ^ punctuation.section.interpolation.end.vue
+//                   ^ punctuation.separator.key-value.html
+//                    ^^^^^^^^^^^^^^^ meta.string.html
+//                    ^ string.quoted.double.html punctuation.definition.string.begin.html
+//                     ^^^^^^^^^^^^^ meta.interpolation.vue source.js.embedded.vue meta.function-call
+//                                  ^ meta.string.html string.quoted.double.html punctuation.definition.string.end.html
+
+<div @[eventName]="doSomething()"></div>
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.tag
+//   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.attribute-with-value.directive.html
+//   ^ entity.other.attribute-name.html keyword.control.directive.vue
+//    ^^^^^^^^^^^ meta.interpolation.vue
+//    ^ punctuation.section.interpolation.begin.vue
+//     ^^^^^^^^^ source.js.embedded.vue variable.other.readwrite.js
+//              ^ punctuation.section.interpolation.end.vue
+//               ^ punctuation.separator.key-value.html
+//                ^^^^^^^^^^^^^^^ meta.string.html
+//                ^ string.quoted.double.html punctuation.definition.string.begin.html
+//                 ^^^^^^^^^^^^^ meta.interpolation.vue source.js.embedded.vue meta.function-call
+//                              ^ meta.string.html string.quoted.double.html punctuation.definition.string.end.html
+
+
+<!--
+ Event Modifiers
+
+ https://vuejs.org/guide/essentials/event-handling.html#event-modifiers
+ -->
+
+<!-- the click event's propagation will be stopped -->
+<a v-on:click.stop="doThis()"></a>
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.tag
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.attribute-with-value.directive.html
+// ^^^^^^^^^^^^^^^ entity.other.attribute-name.html
+// ^^^^ keyword.control.directive.vue
+//     ^ punctuation.separator.vue
+//      ^^^^^ variable.parameter.vue
+//           ^ punctuation.separator.vue
+//            ^^^^ constant.other.vue
+//                ^ punctuation.separator.key-value.html
+//                 ^^^^^^^^^^ meta.string.html
+//                 ^ string.quoted.double.html punctuation.definition.string.begin.html
+//                  ^^^^^^^^ meta.interpolation.vue source.js.embedded.vue meta.function-call
+//                          ^ string.quoted.double.html punctuation.definition.string.end.html
+
+<!-- the click event's propagation will be stopped -->
+<a @click.stop="doThis()"></a>
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.tag
+// ^^^^^^^^^^^^^^^^^^^^^^ meta.attribute-with-value.directive.html
+// ^^^^^^^^^^^ entity.other.attribute-name.html
+// ^ keyword.control.directive.vue
+//  ^^^^^ variable.parameter.vue
+//       ^ punctuation.separator.vue
+//        ^^^^ constant.other.vue
+//            ^ punctuation.separator.key-value.html
+//             ^^^^^^^^^^ meta.string.html
+//             ^ string.quoted.double.html punctuation.definition.string.begin.html
+//              ^^^^^^^^ meta.interpolation.vue source.js.embedded.vue meta.function-call
+//                      ^ string.quoted.double.html punctuation.definition.string.end.html
+
+<!-- modifiers can be chained -->
+<a @click.stop.prevent="doThat()"></a>
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.tag
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.attribute-with-value.directive.html
+// ^^^^^^^^^^^^^^^^^^^ entity.other.attribute-name.html
+// ^ keyword.control.directive.vue
+//  ^^^^^ variable.parameter.vue
+//       ^ punctuation.separator.vue
+//        ^^^^ constant.other.vue
+//            ^ punctuation.separator.vue
+//             ^^^^^^^ constant.other.vue
+//                    ^ punctuation.separator.key-value.html
+//                     ^^^^^^^^^^ meta.string.html
+//                     ^ string.quoted.double.html punctuation.definition.string.begin.html
+//                      ^^^^^^^^ meta.interpolation.vue source.js.embedded.vue meta.function-call
+//                              ^ string.quoted.double.html punctuation.definition.string.end.html
