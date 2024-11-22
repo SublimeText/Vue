f164fb4a336534c074f99d8b7ae13ca5e37949b5
deathaxe <deathaxe82@googlemail.com>
2024-11-22 11:19:23 +0100

    Update README
    This commit...
    
    1. copies various "standard" paragraphs from other packages like Astro or Liquid
    2. adds a new preview image with new features like YAML in <i18n> tags.
    3. removes obsolete samples folder as its content is part of syntax tests
       or new preview.

---
+++ preview.vue
@@ -0,0 +1,45 @@
+<i18n>
+	en:
+		greeting: hello
+		welcome: welcome
+</i18n>
+
+<script setup>
+	import MainComponent from '@/components/MainComponent.vue'
+
+	export default {
+		data () {
+			return { msg: 'Hello Vue!' }
+		},
+		render () {
+			return { <p class="greeting">Hello {data.Vue}!</p> }
+		},
+		components: {
+			MainComponent
+		}
+	}
+</script>
+
+<style lang="scss">
+	$font-stack: Helvetica, sans-serif;
+	$primary-color: #333;
+
+	.MainComponent > body {
+		font: 100% $font-stack;
+		color: $primary-color;
+	}
+</style>
+
+<template>
+	<div>
+		<img :class="$logo_class" src="./assets/logo.png">
+		<h1 @click="greet(msg)">
+			{{ msg + '!' }}
+		</h1>
+		<MainComponent/>
+	</div> <!-- .name -->
+</template>
+
+
+
+
--- samples/arrow.vue
+++
@@ -1,7 +0,0 @@
-<!--
-This was a bug in the old implementation.
--->
-
-<template>
-  <div @click="foo => foo()"></div>
-</template>
